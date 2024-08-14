59c79c8f7271be453434c595791a2b1709d0d76e
Lourdes Redondo <lourdesred835@gmail.com>
2024-08-14 18:05:28 -0300

    Primer parcial Frontend III

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1></h1>
+        <form></form>
+        <Card />
+    </div>
+  );
+}
+
+
+export default App;
